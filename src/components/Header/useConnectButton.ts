9732736cd8313645c1d6fc7f89f458964a09169d
yusunghyun <ysh1513@naver.com>
2022-02-18 00:24:15 +0900

    ✨ 모바일 klip로그인 붙히기만함

--- src/components/Header/useConnectButton.ts
+++ src/components/Header/useConnectButton.ts
@@ -1,4 +1,6 @@
-import Caver from 'caver-js';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { getResult, prepare, request } from 'klip-sdk';
 import { MouseEventHandler, useState } from 'react';
 
 interface Return {
@@ -14,6 +16,24 @@ declare global {
       selectedAddress: string;
     };
   }
+  interface prepareParams {
+    bappName: string;
+    successLink: string;
+    failLink: string;
+  }
+  type prepare = {
+    auth: (params: prepareParams) => Promise<any>;
+  };
+  type request = (request_key: string, option?: Record<any, any>) => void;
+  interface getResultReturn {
+    request_key: string;
+    result: {
+      klaytn_address: string;
+    };
+    status: 'completed';
+    expiration_time: number;
+  }
+  type getResult = (request_key: string) => Promise<getResultReturn>;
 }
 
 const MAINNET_NETWORK_ID = 8217;
@@ -40,13 +60,27 @@ export const useConnectButton = (): Return => {
   };
 
   const onClick = async (): Promise<void> => {
-    const loginSuccess = await kaikasLogin();
-    if (loginSuccess === true) {
-      // 사인 로직 구상중
-      // const caver = new Caver(klaytn);
+    const bappName = 'my app';
+    const successLink = 'myApp://...';
+    const failLink = 'myApp://...';
+    const res = await prepare.auth({ bappName, successLink, failLink });
+    if (res.err) {
+      // 에러 처리
+      console.log('res.err : ', res.err);
+    } else if (res.request_key) {
+      // request_key 보관
+      request(res.request_key, () => alert('모바일 환경에서 실행해주세요'));
+      setTimeout(async () => {
+        alert(JSON.stringify(await getResult(res.request_key)));
+      }, 5000);
     }
-    console.log('window.klaytn : ', window.klaytn);
-    return;
+    // const loginSuccess = await kaikasLogin();
+    // if (loginSuccess === true) {
+    //   // 사인 로직 구상중
+    //   // const caver = new Caver(klaytn);
+    // }
+    // console.log('window.klaytn : ', window.klaytn);
+    // return;
   };
 
   return {
